f6461022848f4d31d414947256fcf7ac984b624a
Alexander López <hector.lopez.mt@correo.usa.edu.co>
2021-10-24 10:57:22 -0500

    First commit g26

---
+++ src/test/java/tutoria/domingo/Reto3Tests.java
@@ -0,0 +1,13 @@
+package tutoria.domingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
